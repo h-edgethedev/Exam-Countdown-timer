ef5212b34c905944f3c84557d931ee2080cfc8ff
h-edgethedev <oluwanifemibarber@gmail.com>
2026-03-18 14:50:59 +0100

    Made a minor mistake while coding

--- stopwatch.py
+++ stopwatch.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout,
 from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtGui import QFont, QFontDatabase
 
-
+exam_date= input("")
 class Stopwatch(QWidget):
     def __init__(self):
         super().__init__()
